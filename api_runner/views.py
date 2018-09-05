0d3e99345b3f227bffba87f7b82186fe9ba68a6f
Jian.Wang <wjian2580@163.com>
2018-09-05 23:46:22 +0800

    'keepgoing'

--- api_runner/views.py
+++ api_runner/views.py
@@ -5,11 +5,36 @@ from api_runner import app
 @app.route('/')
 @app.route('/index/')
 def index():
-
 	return render_template('index.html')
 
 @app.route('/api/project_list/')
 def project_list():
-
 	return render_template('project_list.html')
 
+@app.route('/api/add_project/')
+def add_project():
+	return render_template('add_project.html')
+
+@app.route('/api/module_list/')
+def module_list():
+	return render_template('module_list.html')
+
+@app.route('/api/add_module/')
+def add_module():
+	return render_template('add_module.html')
+
+@app.route('/api/case_list/')
+def case_list():
+	return render_template('case_list.html')
+
+@app.route('/api/add_case/')
+def add_case():
+	return render_template('add_case.html')
+
+@app.route('/api/report_list/')
+def report_list():
+	return render_template('report_list.html')
+
+@app.route('/api/settings/')
+def settings():
+	return render_template('settings.html')
