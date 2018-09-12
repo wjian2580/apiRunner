f7e980427d59cc4c068a3d52d6209a17583a3bcb
= <=>
2018-09-12 18:17:52 +0800

    sql

--- api_runner/__init__.py
+++ api_runner/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLAlchemy'] = 'mysql://root:1234@47.94.23.2/HttpRunner'
+db = SQLAlchemy(app)
 
 from api_runner import views
---
+++ api_runner/models.py
@@ -0,0 +1,10 @@
+from api_runner import app,db
+
+classs UserInfo(db.Model):
+
+	id = db.(db.)
+	
+	def __init__(self，username, password, create_time):
+
+
+
