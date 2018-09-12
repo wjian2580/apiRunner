c71f0fb738c27beb23f8f6bd1bd4a763d3f0cafb
= <=>
2018-09-12 18:18:37 +0800

    delete config

--- api_runner/__init__.py
+++ api_runner/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLAlchemy'] = 'mysql://root:1234@47.94.23.2/HttpRunner'
 db = SQLAlchemy(app)
 
 from api_runner import views
